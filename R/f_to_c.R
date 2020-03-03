a2ee2ecdbdf112d942df0c1ae822b6c8067e6824
Yufei Wang <cevwyfiche@gmail.com>
2020-03-02 16:44:20 -0800

    mmmm

---
+++ R/f_to_c.R
@@ -0,0 +1,19 @@
+#' Fahrenheit converter
+#'
+#'
+#' This function converts temperature from Fahrenheit to Celsius
+#'
+#' @param temp_F numeric input indicating temperature in Fahrenheit
+#'
+#' @return Numeric indicating temperature \code{temp_F} converted to Celsius
+#'
+#' @examples
+#' f_to_c(32)
+#' f_to_c(212)
+#'
+#' @export
+
+f_to_c <- function(temp_F) {
+  temp_C <- (temp_F - 32) * 5 / 9
+  return(temp_C)
+}
