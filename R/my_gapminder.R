db2a68b0f552b9f4b5183910ea9d60f2f02f663d
Yufei Wang <cevwyfiche@gmail.com>
2020-03-12 16:11:45 -0700

    update

---
+++ R/my_gapminder.R
@@ -0,0 +1,23 @@
+#' Jennifer Bryan's gapminder Data
+#'
+#' The famous \code{\link[datasets]{gapminder}} dataset taken from the
+#' \code{datasets} package to demonstrate use of all of these functions.
+#'
+#' @format a data frame with 1704 observations and 6 variables:
+#' \describe{
+#' \item{country}{country name}
+#' \item{continent}{continent name}
+#' \item{year}{from 1952 to 2007}
+#' \item{lifeExp}{life expectancy at birth}
+#' \item{pop}{total population}
+#' \item{gdpPercap}{per-capita GDP}
+#' }
+#'
+#' \source
+#' https://github.com/jennybc/gapminder
+#'
+#'#' @examples
+#' head(my_gapminder)
+#'
+#' The data were collected by Jennifer Bryan.
+"my_gapminder"
---
+++ R/my_rf_cv.R
@@ -0,0 +1,32 @@
+#' Random Forest Cross Validation Function
+#'
+#' This function is to predict output using covariates.
+#' @param k number of folds
+#' @keywords Random Forest Cross-Validation
+#'
+#' @return a list with a numeric with the cross-validation error
+#'
+#' @examples
+#' my_rf_cv(5)
+#'
+#' @import class randomForest dplyr
+#'
+#' @export
+#'
+my_rf_cv <- function(k){
+  set.seed(229)
+  n <- nrow(my_gapminder)
+  fold <- sample(rep(1:k, length = n))
+  data <- data.frame(my_gapminder, "split" = fold)
+  cv_err_vec <- rep(NA, k)
+  for (i in 1:k) {
+    data_train <- data %>% dplyr::filter(split != i)
+    data_test <- data %>% dplyr::filter(split == i)
+    model <- randomForest(lifeExp ~ gdpPercap, data = data_train, ntree = 100)
+    prediction <- predict(my_model, data_test[, -1])
+    cv_err_vec[i] = mean((data_test$lifeExp - prediction)^2)
+  }
+  cv_err <- mean(cv_err_vec)
+  return(cv_err)
+}
+
